a0caaef906b4290e2949eba78cb864f35c9a3453
Marlon Knupp <marlonjcc23@gmail.com>
2024-08-29 21:39:13 -0300

    ...

---
+++ Gráficos.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+
+categorias = ['A', 'B', 'C', 'D']
+valores = [40, 30, 55, 79]
+
+
+plt.figure(num="Figura de Exemplo para Gráfico de Barras")
+plt.bar(categorias, valores , color='skyblue')
+plt.title('Gráfico de Barras')
+plt.xlabel('Categorias')
+plt.ylabel('Valores')
+
+
+plt.show()
